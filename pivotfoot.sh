18828550825c761cb164c3b2ddfc09fbd3d1040d
Toño Díaz <jupyterjones@pm.me>
2021-12-17 18:36:20 +0100

    Added pivot foot

---
+++ pivotfoot.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "  + EXECUTING SABONIS. LET'S FORENSICATE!..."
+evtx_dump "${thisevtxf}/Security.evtx" -f "${modod}/Security.evtx.xml" --dont-show-record-number --no-indent
+[ -f "${thisevtxf}/Microsoft-Windows-SMBServer%4Security.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-SMBServer>
+[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-RDPClient%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Microso>
+[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" ] && evtx_dump "${this>
+[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx" ] && evtx_dump "${thisevtx>
+[ -f "${thisevtxf}/Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Mi>
+[ -f "${thisevtxf}/Microsoft-Windows-SmbClient%4Security.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-SmbClient>
+sed -i 's+<?xml version="1.0" encoding="utf-8"?>++' ${modod}/*.xml
+sed -i '1s;^;<?xml version="1.0" encoding="utf-8"?><Events>;' ${modod}/*.xml
+echo -e "</Events>"  | tee -a ${modod}/*.xml
+sabonis.py parse evtx --source_artifact $modod --directory --csv_output $thisprocessof --ignore_local
