fd8c8ece0bb7da34b5f0dfdb084d5a7ad4076c18
Antonio Diaz <88971898+jupyterj1s@users.noreply.github.com>
2021-12-18 12:20:56 +0100

    Update pivotfoot.sh

--- pivotfoot.sh
+++ pivotfoot.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "  + EXECUTING SABONIS. LET'S FORENSICATE!..."
 evtx_dump "${thisevtxf}/Security.evtx" -f "${modod}/Security.evtx.xml" --dont-show-record-number --no-indent
-[ -f "${thisevtxf}/Microsoft-Windows-SMBServer%4Security.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-SMBServer>
-[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-RDPClient%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Microso>
-[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" ] && evtx_dump "${this>
-[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx" ] && evtx_dump "${thisevtx>
-[ -f "${thisevtxf}/Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Mi>
-[ -f "${thisevtxf}/Microsoft-Windows-SmbClient%4Security.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-SmbClient>
+[ -f "${thisevtxf}/Microsoft-Windows-SMBServer%4Security.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-SMBServer%4Security.evtx" --dont-show-record-number --no-indent
+[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-RDPClient%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-TerminalServices-RDPClient%4Operational.evtx" --dont-show-record-number --no-indent
+[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-TerminalServices-RemoteConnectionManager%4Operational.evtx" --dont-show-record-number --no-indent
+[ -f "${thisevtxf}/Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-TerminalServices-LocalSessionManager%4Operational.evtx" --dont-show-record-number --no-indent
+[ -f "${thisevtxf}/Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-RemoteDesktopServices-RdpCoreTS%4Operational.evtx" --dont-show-record-number --no-indent
+[ -f "${thisevtxf}/Microsoft-Windows-SmbClient%4Security.evtx" ] && evtx_dump "${thisevtxf}/Microsoft-Windows-SmbClient%4Security.evtx" --dont-show-record-number --no-indent
 sed -i 's+<?xml version="1.0" encoding="utf-8"?>++' ${modod}/*.xml
 sed -i '1s;^;<?xml version="1.0" encoding="utf-8"?><Events>;' ${modod}/*.xml
 echo -e "</Events>"  | tee -a ${modod}/*.xml
